2741122d499f094bb62b5b16b3a6a46d25d922ac
umiumi19 <miu6502@gmail.com>
2026-06-17 15:20:31 +0900

    rename folder

--- 01-db-access/ent_tutorial/ent/runtime/runtime.go
+++ 01-db-access/ent_tutorial/ent/runtime/runtime.go
@@ -2,7 +2,7 @@
 
 package runtime
 
-// The schema-stitching logic is generated in spike/01-db-access/ent/runtime.go
+// The schema-stitching logic is generated in spike/01-db-access/ent_tutorial/ent/runtime.go
 
 const (
 	Version = "v0.14.6"                                         // Version of ent codegen.
